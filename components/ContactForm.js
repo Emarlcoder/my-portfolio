059127158d0eac1e11250281f2ecad45f1be3161
Ema Romero López <emarlcoder@gmail.com>
2021-11-03 10:31:07 -0300

    Adding Netlify

---
+++ components/ContactForm.js
@@ -0,0 +1,63 @@
+
+
+const ContactForm = () => {
+  return (
+    <>
+      <h1>Contact Me!</h1>
+      <form className="row g-3" name="portfolio-contact-form" netlify>
+        <div className="col-md-6">
+          <label for="firstName" className="form-label">
+            First Name
+          </label>
+          <input type="text" className="form-control" id="firstName" required />
+        </div>
+        <div className="col-md-6">
+          <label for="lastName" className="form-label">
+            Last Name
+          </label>
+          <input type="text" className="form-control" id="lastName" required />
+        </div>
+        <div className="col-md-8">
+          <label for="emailInfo" className="form-label">
+            E-mail
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="emailInfo"
+            required
+          />
+        </div>
+        <div className="col-md-4">
+          <label for="phoneNumber" className="form-label">
+            Phone Number
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="phoneNumber"
+            placeholder="+54 (11) 1111-1111"
+          />
+        </div>
+        <div className="col-md-12">
+          <label for="comments" className="form-label">
+            Comments, questions?
+          </label>
+          <textarea
+            className="form-control"
+            id="comments"
+            rows="3"
+            required
+          ></textarea>
+        </div>
+        <div className="col-md-12">
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </div>
+      </form>
+    </>
+  );
+}
+
+export default ContactForm;
--- pages/contact.js
+++ pages/contact.js
@@ -1,4 +1,5 @@
 import Layout from "../components/Layout";
+import ContactForm from "../components/ContactForm";
 
 const Contact = () => (
   <Layout footer={false}>
@@ -12,32 +13,7 @@ const Contact = () => (
       </div>
       <div className="col-1 bg-light"></div>
       <div className="col-6 bg-dark text-light py-5">
-          <h1>Contact Me!</h1>
-          <form className="row g-3">
-            <div className="col-md-6">
-              <label for="firstName" className="form-label">First Name</label>
-              <input type="text" className="form-control" id="firstName" required/>
-            </div>
-            <div className="col-md-6">
-              <label for="lastName" className="form-label">Last Name</label>
-              <input type="text" className="form-control" id="lastName" required/>
-            </div>
-            <div className="col-md-8">
-              <label for="emailInfo" className="form-label">E-mail</label>
-              <input type="email" className="form-control" id="emailInfo" required/>
-            </div>
-            <div className="col-md-4">
-              <label for="phoneNumber" className="form-label">Phone Number</label>
-              <input type="text" className="form-control" id="phoneNumber" placeholder="+54 (11) 1111-1111"/>
-            </div>
-            <div className="col-md-12">
-              <label for="comments" className="form-label">Comments, questions?</label>
-              <textarea className="form-control" id="comments" rows="3" required></textarea>
-            </div>
-            <div className="col-md-12">
-              <button type="submit" className="btn btn-primary">Submit</button>
-            </div>
-          </form>
+        <ContactForm />
       </div>
     </div>
   </Layout>
